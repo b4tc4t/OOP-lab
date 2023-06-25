15f8295eb5d06b841ed49f12d6a46b42888cf737
b4tc4t <117244959+b4tc4t@users.noreply.github.com>
2023-06-25 23:48:28 +0700

    Add files via upload

---
+++ Lab02/AimsProject/DigitalVideoDisc.java
@@ -0,0 +1,63 @@
+package AimsProject;
+
+public class DigitalVideoDisc {
+    private String title;
+    private String category;
+    private String director;
+    private int length;
+    private float cost;
+
+    public String getTitle() {
+        return title;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getDirector() {
+        return director;
+    }
+    public int getLength() {
+        return length;
+    }
+    public float getCost() {
+        return cost;
+    }
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+    public DigitalVideoDisc(String title, String category, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String title, String category, String director, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;
+        this.cost = cost;
+    }
+
+    public static void main(String[] args){
+        Cart anOrder = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation",
+                "Roger Allers", 87, 19.95f);
+        anOrder.addDigitalVideoDisc(dvd1);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction",
+                "George Lucas", 87, 24.95f);
+        anOrder.addDigitalVideoDisc(dvd2);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation",
+                 18.99f);
+        anOrder.addDigitalVideoDisc(dvd3);
+
+        System.out.print("Total Cost is: ");
+        System.out.println(anOrder.totalCost());
+    }
+}
